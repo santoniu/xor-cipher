ba57e32e30d717ba0dc8ef322c580794a1ca4822
Antoniu-George Savu <antoniu.savu@splend.com>
2023-02-13 15:12:53 +0000

    * added -o <output_filename>. * overwriting original file if no -o is given.

--- xor_file.c
+++ xor_file.c
@@ -14,9 +14,10 @@ int main(int argc, char **argv) {
     extern char *optarg;
     extern int optind, opterr, optopt;
 
+    char *output_filename = NULL;
     int xor_key_interval_start = 0;
     int xor_key_interval_length = XOR_KEY_LENGTH_IN_BYTES;
-    while ( (opt = getopt(argc, argv, "cg:hl:s:")) != -1 ) {
+    while ( (opt = getopt(argc, argv, "cg:hl:o:s:")) != -1 ) {
         switch (opt) {
 	    case 'c':
 		fprintf(stdout, "XOR key CRC: 0x%016X\n", xor_algo_key_crc());
@@ -27,9 +28,12 @@ int main(int argc, char **argv) {
             case 's':
 		xor_key_interval_start = atoi(optarg);
                 break;
+	    case 'o':
+		output_filename = optarg;
+		break;
 	    case 'h': /* fall-through */
             default:
-                fprintf(stderr, "Usage:\n%s [-h] [-s <interval_start_offset> ] [-l <interval_length>] [-g <segments>] <file_name>\n", argv[0]);
+                fprintf(stderr, "Usage:\n%s [-c] [-g <segments>] [-h] [-l <interval_length>] [-o <output_filename>] [-s <interval_start_offset>] <input_filename>\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
@@ -49,64 +53,68 @@ int main(int argc, char **argv) {
 	xor_key_interval_length = XOR_KEY_LENGTH_IN_BYTES-xor_key_interval_start;
     }
 
-    const char *filename = argv[optind];
+    char *input_filename = argv[optind];
     struct stat sb;
-    if ( stat(filename, &sb) == -1) {
-	fprintf(stderr, "ERROR: can not stat() file '%s'\n", filename);
+    if ( stat(input_filename, &sb) == -1) {
+	fprintf(stderr, "ERROR: can not stat() file '%s'\n", input_filename);
 	exit(EXIT_FAILURE);
     }
 
     if ( S_ISREG(sb.st_mode) == 0 ) {
-	fprintf(stderr, "ERROR: '%s' is not a regular file.\n", filename);
+	fprintf(stderr, "ERROR: '%s' is not a regular file.\n", input_filename);
 	exit(EXIT_FAILURE);
     }
 
-    int fin = open(filename, O_RDONLY);
-    if ( fin == -1 ) {
-	fprintf(stderr, "ERROR: can not open file '%s' for reading\n", filename);
+    int fd_in = open(input_filename, O_RDONLY);
+    if ( fd_in == -1 ) {
+	fprintf(stderr, "ERROR: can not open input file '%s' for reading\n", input_filename);
 	exit(EXIT_FAILURE);
     }
 
-    char *tmp_filename = (char *) malloc(strlen(filename)+5);
-    memset(tmp_filename, 0, strlen(filename)+5);
-    snprintf(tmp_filename, strlen(filename)+5, "%s.tmp", filename);
-
-    int fout = creat(tmp_filename, sb.st_mode);
-    if ( fout == -1 ) {
-	close(fin);
-	fprintf(stderr, "ERROR: can not create temporary output file\n");
+    if ( output_filename == NULL ) 
+	output_filename = input_filename;
+    
+    int fd_out = open(output_filename, O_CREAT|O_WRONLY);
+    if ( fd_out == -1 ) {
+	close(fd_in);
+	fprintf(stderr, "ERROR: can not open output file '%s' for reading\n", output_filename);
 	exit(EXIT_FAILURE);
     }
 
     unsigned char io_buffer[1024];
     int read_bytes;
     unsigned int xor_key_position = 0;
-    while ( (read_bytes = read(fin, &io_buffer, sizeof(io_buffer))) > 0 ) {
+    while ( (read_bytes = read(fd_in, &io_buffer, sizeof(io_buffer))) > 0 ) {
 	if ( xor_algo(&io_buffer, read_bytes, xor_key_interval_start, xor_key_interval_length, &xor_key_position) != XOR_ALGO_OK ) {
 	    fprintf(stderr, "ERROR: xor_algo failed\n");
-	    close(fin);
-	    close(fout);
-	    unlink(tmp_filename);
+	    close(fd_in);
+	    close(fd_out);
 	    exit(EXIT_FAILURE);
 	}
-	if ( write(fout, &io_buffer, read_bytes) < 0 ) {
-	    fprintf(stderr, "ERROR: write() failed\n");
-	    close(fin);
-	    close(fout);
-	    unlink(tmp_filename);
+	if ( write(fd_out, &io_buffer, read_bytes) < 0 ) {
+	    if ( input_filename == output_filename )
+		fprintf(stderr, "ERROR: can not write into output filename '%s'. Your original file was lost.\n", output_filename);		
+	    else
+		fprintf(stderr, "ERROR: can not write into output filename '%s'\n", output_filename);
+	    close(fd_in);
+	    close(fd_out);
 	    exit(EXIT_FAILURE);
 	}
     }
 
-    close(fin);
-    close(fout);
+    close(fd_in);
+    close(fd_out);
 
-    if ( unlink(filename) < 0 ) {
-	fprintf(stderr, "ERROR: can not remove filename '%s'\n", filename);
-	unlink(tmp_filename);
+    if ( read_bytes == -1 ) {
+	fprintf(stderr, "ERROR: can not read from input filename '%s'\n", input_filename);
 	exit(EXIT_FAILURE);
     }
 
-    rename(tmp_filename, filename);
+    if ( chown(output_filename, sb.st_uid, sb.st_gid) < 0 )
+	fprintf(stderr, "WARNING: can not set ownership on output filename '%s'\n", output_filename);
 
+    if ( chmod(output_filename, sb.st_mode) < 0 )
+	fprintf(stderr, "WARNING: can not set permissions on output filename '%s'\n", output_filename);
+
+    exit(EXIT_SUCCESS);
 }
